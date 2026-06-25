0ccb4e252d8abe315780e3d43642a826dce50963
Salman Qureshi <salman.qureshi.riaz@gmail.com>
2026-06-25 10:44:23 +0500

    Add Security Features

---
+++ src/main/java/com/CrudDemoApplication/security/DemoSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.CrudDemoApplication.security;
+
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails Maroof= User.builder()
+                .username("maroof")
+                .password("{noop}1234")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails Salman= User.builder()
+                .username("salman")
+                .password("{noop}1234")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+        UserDetails Anees= User.builder()
+                .username("anees")
+                .password("{noop}1234")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(Maroof,Salman,Anees);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        httpSecurity
+                .authorizeHttpRequests( configure ->
+                configure
+                        .requestMatchers(HttpMethod.GET,"/api/employee").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employee/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employee").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employee").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employee/**").hasRole("ADMIN")
+
+                );
+        //use Http basic authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        //disable csrf due to not use in Rest Api Put,Post,Delete,Patch
+        httpSecurity.csrf(csrf ->
+            csrf.disable());
+        return httpSecurity.build();
+    }
+}
